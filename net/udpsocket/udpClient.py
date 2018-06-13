8754091afc7dab7f11ffe149b211133da06ce9c8
pythonCat <pythoncat@qq.com>
2018-06-13 23:16:44 +0800

    change dir

---
+++ net/udpsocket/udpClient.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @name   : udpClient.py
+# @author : cat
+# @date   : 2018/6/13.
+
+
+if __name__ == '__main__':
+    pass
---
+++ net/udpsocket/udpServer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @name   : udpServer.py
+# @author : cat
+# @date   : 2018/6/13.
+
+
+if __name__ == '__main__':
+    pass
