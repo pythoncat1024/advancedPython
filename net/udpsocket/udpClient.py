d80cac4ee4881978b1d016f5c2772526e45a297b
pythonCat <pythoncat@qq.com>
2018-06-13 23:17:46 +0800

    udp-ip topic

--- net/udpsocket/udpClient.py
+++ net/udpsocket/udpClient.py
@@ -4,6 +4,29 @@
 # @author : cat
 # @date   : 2018/6/13.
 
+import socket
+
+
+def start_client():
+    HOST = 'localhost'  # 表示可以绑定任意 ip
+    PORT = 26375  # 指定端口
+    BUFFER_SIZE = 1024
+    ADDR = (HOST, PORT)
+    client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    while True:
+        msg = str(input(">: "))
+        if not msg:
+            break
+        client.sendto(bytes(msg, 'utf-8'), ADDR)
+
+        msg, addr = client.recvfrom(BUFFER_SIZE)
+
+        if not msg:
+            break
+        print(str(msg, 'utf-8'))
+
 
 if __name__ == '__main__':
+    start_client()
     pass
--- net/udpsocket/udpServer.py
+++ net/udpsocket/udpServer.py
@@ -4,6 +4,31 @@
 # @author : cat
 # @date   : 2018/6/13.
 
+import socket
+import time
+
+
+def run_server():
+    HOST = ''  # 表示可以绑定任意 ip
+    PORT = 26375  # 指定端口
+    BUFFER_SIZE = 1024
+    ADDR = (HOST, PORT)
+    RUNNING_SERVICE = True
+
+    server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server.bind(ADDR)
+
+    while RUNNING_SERVICE:
+        print("waiting for message...")
+        message, client_addr = server.recvfrom(BUFFER_SIZE)  # 不同于 tcp ，并没有 accept
+
+        msg = "[{}]:{}".format(time.ctime(), str(message, 'utf-8'))
+        server.sendto(bytes(msg, 'utf-8'), client_addr)
+        print("received from and returned to {addr}".format(addr=client_addr))
+    server.close()
+    pass
+
 
 if __name__ == '__main__':
+    run_server()
     pass
