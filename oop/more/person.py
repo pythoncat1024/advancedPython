f36c2d198f9a4e9c453c398d42d4f971c8102d9e
pythonCat <pythoncat.cheng@gometech.com.cn>
2018-06-21 14:25:20 +0800

    典型 oop 中的继承多态的实现

--- oop/more/person.py
+++ oop/more/person.py
@@ -20,10 +20,94 @@ class Person:
         self.pay = pay
         pass
 
+    def __str__(self) -> str:
+        return "Person:(name={},job={},pay={})".format(self.name, self.job, self.pay)
+
+    def giveRaise(self, percent):
+        self.pay *= (1 + percent)
+        pass
+
+
+class Manager(Person):
+    """
+    基于继承的方式去实现
+    """
+
+    def __init__(self, name, pay=0):
+        Person.__init__(self, name, 'mgr', pay)
+
+    def giveRaise(self, percent, bounds=0.1):
+        # todo: 这里 第三个参数必须提供默认值，否则就不是重载了
+        return Person.giveRaise(self, percent + bounds)
+
+
+class Boss:
+    """
+    基于组合的实现===> 委托代码模式 [ __attr__ / getattr ]
+    """
+
+    def __init__(self, name, pay):
+        self.person = Person(name, 'ceo', pay)
+
+    def giveRaise(self, percent, bounds=.3):
+        self.person.giveRaise(percent + bounds)
+
+    def __getattr__(self, item):
+        return getattr(self.person, item)
+
+    def __str__(self):
+        return str(self.person)
+
+
+class Department:
+    """
+    聚合 ===> 继承和组合的综合使用
+    """
+
+    def __init__(self, *args):
+        # print(args) # 得到的是一个 元组
+        self.members = list(args)
+
+    def addMember(self, person):
+        self.members.append(person)
+
+    def giveRaise(self, percent):
+        """
+        todo: 这个有意思了，如果是 Person 就 + 0.1 ，Manager +0.3 ,Boss + 0.4，而且是自动的，这一切是来源于继承，多态
+        :param percent: 加钱比例 (0,1)
+        :return: NONE
+        """
+        for person in self.members:
+            person.giveRaise(percent)
+
+    def showMembers(self):
+        print("==>[")
+        for person in self.members:
+            print(person)
+        print("]")
+
 
 if "__main__" == __name__:
     bob = Person('bob')
     tom = Person('tom', pay=66, job='writer')
 
-    print(bob.name, tom.pay)
+    stone = Manager('stone', 5000)
+    print(bob, tom, stone, sep='\n')
+    print("------------------------------")
+    stone.giveRaise(0.1)
+    print(bob, tom, stone, sep='\n')
+
+    print("==============================")
+
+    boss = Boss('victor', 99 + 1)
+    print(boss)
+    boss.giveRaise(.1)
+    print(boss)
+
+    print("++++++++++++++++++++++++++++")
+    dep = Department(boss, bob, tom)
+    dep.addMember(stone)
+    dep.showMembers()
+    dep.giveRaise(0.1)
+    dep.showMembers()
     pass
